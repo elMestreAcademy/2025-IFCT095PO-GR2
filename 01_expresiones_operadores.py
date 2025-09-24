63c3d4a7c240320cbdc12f64c781ce541a060470
elMestre <carmesce@gmail.com>
2025-09-24 10:16:05 +0200

    Ejemplos python

---
+++ 01_expresiones_operadores.py
@@ -0,0 +1,19 @@
+# Operaciones aritméticas
+3 + 2
+7 * 3
+10 / 3
+200 ** 2
+100000000 * 2 - 4156
+
+# Orden de resolución de expresiones
+2 + 3 * 2
+(2 + 3) * 2
+
+# Asignacion de variables
+a = 7
+b = 5
+c = a + b
+
+print("SALIDA:           **")
+print(">>>>>>>", 4, "<<<<<<<<<<")
+print("====================")
