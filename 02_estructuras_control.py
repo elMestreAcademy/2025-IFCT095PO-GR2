192ca88d3c504ce62f0e87ef040c183baaf7f0f4
elMestre <carmesce@gmail.com>
2025-10-01 10:04:35 +0200

    sync

---
+++ 02_estructuras_control.py
@@ -0,0 +1,14 @@
+x = 10
+acumulado = 1000
+
+print("Instruccion 1")
+print("Instruccion 2")
+print("Instruccion 3")
+
+if x > 5:
+    print("Se ejecuta la parte del if")
+    acumulado = acumulado * 2
+
+print("Instruccion 5")
+print("Instruccion 6")
+print("aculumaldo: ", acumulado)
